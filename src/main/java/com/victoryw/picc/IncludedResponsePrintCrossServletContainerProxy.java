47903ad1a87085cbd7aa025a58b2dc49d531672b
tianyawy <tianya_wy@hotmail.com>
2019-04-07 17:40:19 +0800

    add the comment

--- src/main/java/com/victoryw/picc/IncludedResponsePrintCrossServletContainerProxy.java
+++ src/main/java/com/victoryw/picc/IncludedResponsePrintCrossServletContainerProxy.java
@@ -1,9 +1,27 @@
 package com.victoryw.picc;
 
 import javax.servlet.ServletContext;
+import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.xml.ws.ResponseWrapper;
 import java.io.IOException;
 
+/**
+ * This class is used to make the compatibility of WebLogic and Jetty, When print string in the
+ * {@link javax.servlet.RequestDispatcher#include(ServletRequest, ServletResponse)} servlet
+ *
+ * <p>This is from the issue: <br/>
+ * When use the {@link javax.servlet.ServletResponseWrapper} to wrap the {@link javax.servlet.ServletResponse}
+ * and  {@link javax.servlet.RequestDispatcher#include(ServletRequest, ServletResponse)} other servlet in the jsp
+ * {@code  <jsp:include page="/OtherServlet" />}, the WebLogic will pass the ServletResponseWrapper
+ * instead of its own internal type Response object to the included servlet,
+ * there will throw exception by the WebLogic jsp render if you use the {@link ServletResponse#getWriter()} in the included servlet.
+ * But because in the same case, there also will throw exception if we change the code to  {@link ServletResponse#getOutputStream()} and run in the jetty
+ * So the class is help us to print string correctly by choose the method according to the servlet container.
+ * </p>
+ *
+ * @author wang victory yan
+ */
 class IncludedResponsePrintCrossServletContainerProxy {
     private ResponseWriter responsePrintWriter;
 
